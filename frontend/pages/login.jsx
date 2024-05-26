af1c7230ee19ab24c43371e1f8090973858da211
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-05-26 21:17:12 +0530

    Update login.jsx

--- frontend/pages/login.jsx
+++ frontend/pages/login.jsx
@@ -14,7 +14,7 @@ export default function login() {
             toast.error('All fields are required!')
         }
         else{
-            const response=await axios.post('https://challenges-presidioback-8nd8qahl2-rohits-projects-a5c6d24a.vercel.app/api/login',{email:email,password:password})
+            const response=await axios.post('https://presidio-backend-1.onrender.com/api/login',{email:email,password:password})
             const {message,ud}=response.data
             if(message=='Login Failed'){
                 toast.error('Login Failed')
